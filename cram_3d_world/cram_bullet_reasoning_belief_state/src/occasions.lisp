36e08dee06a3430af4398938bb3833e99f3b5b5d
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2019-01-18 16:43:50 +0100

    Fix for object-designator-name.

--- cram_3d_world/cram_bullet_reasoning_belief_state/src/occasions.lisp
+++ cram_3d_world/cram_bullet_reasoning_belief_state/src/occasions.lisp
@@ -61,7 +61,7 @@
 
 (defun desig-has-data (desig)
   (with-slots ((data desig:data)) desig
-    data))
+    (and data (typep data 'desig:object-designator-data))))
 
 (def-fact-group occasion-utilities (object-designator-name desig:desig-location-prop)
   (<- (object-designator-name ?name ?name)
@@ -73,9 +73,10 @@
         (and (not (bound ?object-designator))
              (lisp-fun unique-object-designators ?object-designators)
              (member ?one-object-designator-from-chain ?object-designators)
-             (desig:current-designator ?one-object-designator-from-chain ?object-designator)
-             (lisp-pred desig-has-data ?object-designator)))
-    (lisp-fun get-designator-object-name ?object-designator ?belief-name)
+             (desig:current-designator ?one-object-designator-from-chain ?object-designator)))
+    (or (and (lisp-pred desig-has-data ?object-designator)
+             (lisp-fun get-designator-object-name ?object-designator ?belief-name))
+        (equal ?belief-name nil))
     (-> (lisp-pred identity ?belief-name)
         (equal ?object-name ?belief-name)
         (and (desig:desig-prop ?object-designator (:type ?object-type))
