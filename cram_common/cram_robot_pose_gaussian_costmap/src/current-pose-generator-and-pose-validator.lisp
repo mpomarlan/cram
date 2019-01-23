4150b04f90647e56b2f8f8c18113f97bd65f53ee
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2019-01-23 12:50:01 +0100

    Reachability validation function will now call IK service, instead of using distance thresholds.

--- cram_common/cram_robot_pose_gaussian_costmap/src/current-pose-generator-and-pose-validator.lisp
+++ cram_common/cram_robot_pose_gaussian_costmap/src/current-pose-generator-and-pose-validator.lisp
@@ -68,35 +68,59 @@
 
 (defun reachable-location-validator (designator pose)
   (if (cram-robot-interfaces:reachability-designator-p designator)
-      (cut:with-vars-bound (?to-reach-point ?min-distance ?max-distance
+      (cut:with-vars-bound (?target-specs ?arms ?min-distance ?max-distance ?base-frame
                                             ?orientation-samples ?orientation-sample-step)
           (cut:lazy-car
            (prolog:prolog
-            `(and (bagof ?pose
-                         (cram-robot-interfaces:designator-reach-pose ,designator ?pose ?_)
-                         ?poses)
-                  (lisp-fun costmap:2d-pose-covariance ?poses 0.5 (?to-reach-point ?_))
+            `(and (bagof (?pose ?arm)
+                         (cram-robot-interfaces:designator-reach-pose ,designator ?pose ?arm)
+                         ?target-specs)
+                  (cram-robot-interfaces:robot ?robot)
+                  (findall ?a (cram-robot-interfaces:arm ?robot ?a) ?arms)
+                  (cram-robot-interfaces:robot-base-frame ?robot ?base-frame)
                   (costmap:costmap-reach-minimal-distance ?min-distance)
                   (costmap:costmap-in-reach-distance ?max-distance)
                   (costmap:orientation-samples ?orientation-samples)
                   (costmap:orientation-sample-step ?orientation-sample-step))))
-        (if (or (cut:is-var ?to-reach-point)
+        (if (or (cut:is-var ?target-specs)
                 (cut:is-var ?min-distance)
                 (cut:is-var ?max-distance))
             :unknown
-            (let ((dist (cl-transforms:v-dist ?to-reach-point (cl-transforms:origin pose)))
-                  (perfect-angle (costmap::angle-to-point-direction
-                                  (cl-transforms:x (cl-transforms:origin pose))
-                                  (cl-transforms:y (cl-transforms:origin pose))
-                                  ?to-reach-point))
-                  (allowed-range (* ?orientation-sample-step
-                                    (ceiling (/ ?orientation-samples 2))))
-                  (generated-angle (calculate-z-angle pose)))
-              (if (and (< dist ?max-distance)
-                       (> dist ?min-distance)
-                       (<= (abs (- perfect-angle generated-angle)) allowed-range))
-                  :accept
-                  :reject))))
+            ;; tests that SOME pose in the designator is reachable by the required arm(s)
+            (if (remove-if #'null
+                           (mapcar (lambda (tspec)
+                                     (let* ((target-pose (first tspec))
+                                            (target-pose (cl-tf:transform-pose cram-tf:*transformer*
+                                                                               :pose target-pose
+                                                                               :target-frame "map"))
+                                            (arm (second tspec))
+                                            (base-in-map (cl-tf:make-transform-stamped "map" ?base-frame 0
+                                                                                       (cl-tf:origin pose)
+                                                                                       (cl-tf:orientation pose)))
+                                            (target-in-map (cl-tf:make-transform-stamped (cl-tf:frame-id target-pose) "target" 0
+                                                                                         (cl-tf:origin target-pose)
+                                                                                         (cl-tf:orientation target-pose)))
+                                            (target-in-base (cl-tf:transform* (cl-tf:transform-inv base-in-map) target-in-map))
+                                            (target-pose (cl-tf:make-pose-stamped ?base-frame 0
+                                                                                  (cl-tf:translation target-in-base)
+                                                                                  (cl-tf:rotation target-in-base))))
+                                       (if (and arm (symbolp arm))
+                                         ;; there is one arm specified for the target: so check that it can reach it
+                                         (cram-robot-interfaces:compute-iks target-pose :arm arm)
+                                         (if (and arm (listp arm))
+                                           ;; there are several arms specified for the target: check that ALL can reach it
+                                           (not (member nil
+                                                        (mapcar (lambda (arm)
+                                                                  (cram-robot-interfaces:compute-iks target-pose :arm arm))
+                                                                arm)))
+                                           ;; there is no arm specified for the target: check that ANY can reach it
+                                           (remove-if #'null
+                                                      (mapcar (lambda (arm)
+                                                                (cram-robot-interfaces:compute-iks target-pose :arm arm))
+                                                              (cut:force-ll ?arms)))))))
+                                   (cut:force-ll ?target-specs)))
+              :accept
+              :reject)))
       :unknown))
 
 (desig:register-location-validation-function
